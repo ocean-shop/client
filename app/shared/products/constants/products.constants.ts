3042bc53456f3292ab08d65e6ecf271bdbe0805f
tkuku <taras.kukuliak@aravo.com>
2026-09-17 11:16:14 +0300

    feat: Use .env functionality

--- app/shared/products/constants/products.constants.ts
+++ app/shared/products/constants/products.constants.ts
@@ -1,4 +1,3 @@
-export const POPULAR_PRODUCTS_API_URL =
-  "https://api-production-1765.up.railway.app/catalog/products/popular/";
+export const POPULAR_PRODUCTS_API_URL = `${process.env.API_BASE_URL}/catalog/products/popular?shopId=${process.env.SHOP_ID}`;
 
 export const PRODUCT_CARD_DEFAULT_CTA_LABEL = "Додати в кошик";
